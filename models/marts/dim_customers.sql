13dd796dd339c516424a29b4e56e024edec945b7
vitorgewe <vitorgewe@gmail.com>
2021-08-10 01:15:06 -0300

    Added first and last names in customers dimension table.

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,9 +1,22 @@
 with 
     customers as (
         select
-            {{ dbt_utils.surrogate_key('customer_id') }} as customer_sk
-            , *
-        from {{ ref('stg_customers')}}
+            /* Surrogate key */
+            {{ dbt_utils.surrogate_key('customers2.customer_id') }} as customer_sk
+
+            /* Primary key */
+            , customers2.customer_id
+
+            /* Foreign keys */
+            , customers2.person_id
+            , customers2.territory_id
+
+            /* Columns to use in marts */
+            , people.first_name
+            , people.last_name
+        from {{ ref('stg_customers')}} as customers2
+        left join {{ ref('stg_people') }} as people
+        on customers2.person_id = people.business_entity_id
     )
 
 select * from customers
