91d51ce389f85ac7c1d1f36780f791a88b96bcef
vitorgewe <vitorgewe@gmail.com>
2021-08-10 01:18:59 -0300

    Fixed an error that was causing to get granurality diferent than expected.

--- models/marts/fact_sales.sql
+++ models/marts/fact_sales.sql
@@ -2,7 +2,7 @@ with
     sales as (
         select
             /* Surrogate key */
-            {{ dbt_utils.surrogate_key('sales_order_header.order_id') }} as order_sk
+            {{ dbt_utils.surrogate_key('sales_order_details.order_detail_id') }} as order_sk
 
             /* Primary key */
             , sales_order_details.order_detail_id
@@ -21,7 +21,6 @@ with
             , sales_order_header.ship_date
             , sales_order_header.due_date
             , credit_cards.card_type
-            , sales_reason.reason_type
             , sales_order_details.order_qty
             , sales_order_details.unit_price
             , sales_order_details.unit_price_discount
@@ -34,10 +33,6 @@ with
         on sales_order_details.order_id = sales_order_header.order_id
         left join {{ ref('stg_credit_cards') }} as credit_cards
         on sales_order_header.credit_card_id = credit_cards.credit_card_id
-        left join {{ ref('stg_sales_order_header_sales_reason') }} as sales_order_header_sales_reason
-        on sales_order_header.order_id = sales_order_header_sales_reason.order_id
-        left join {{ ref('stg_sales_reason') }} as sales_reason
-        on sales_order_header_sales_reason.sales_reason_id = sales_reason.sales_reason_id
     )
 
 select * from sales
