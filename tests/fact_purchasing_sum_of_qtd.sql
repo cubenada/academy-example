939c96beb521859f93ac872a525822862cc10b3f
vitorgewe <vitorgewe@gmail.com>
2021-08-11 15:51:15 -0300

    Fixed tests and deleted unused tests.

--- tests/fact_purchasing_sum_of_qtd.sql
+++
@@ -1,9 +0,0 @@
-with 
-    sum_qty as (
-        select
-            sum(order_qty) as qty
-        from {{ ref('fact_purchasing') }}
-        where due_date between '2012-01-01' and '2012-01-31'
-    )
-
-select * from sum_qty where qty != 13109
